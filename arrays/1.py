4c97185edc78065a602a480470b156f2fda20829
Zachary Flota <zflota1@gmail.com>
2022-07-20 12:19:59 -0400

    completed assignment

---
+++ arrays/1.py
@@ -0,0 +1,8 @@
+import numpy as np
+a = np.array([1,2,3,4])
+
+def individual(array):
+    for x in array:
+        print(x)
+
+individual(a)
--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,7 @@
+import numpy as np
+a = np.array([1,2,3,4])
 
+def mult_three(array):
+    return 3 * array
+
+print(mult_three(a))
